3245ff04177d63abf7f7764ca27a4b81523fbd10
masquernya <127450910+masquernya@users.noreply.github.com>
2023-08-02 20:39:30 -0400

    Migrate

---
+++ Disco.Web/Migrations/20230802235125_AddMatrix.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Disco.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMatrix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "accountMatrix",
+                columns: table => new
+                {
+                    accountMatrixId = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    accountId = table.Column<long>(type: "INTEGER", nullable: false),
+                    name = table.Column<string>(type: "TEXT", nullable: false),
+                    domain = table.Column<string>(type: "TEXT", nullable: false),
+                    avatarUrl = table.Column<string>(type: "TEXT", nullable: true),
+                    createdAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    updatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_accountMatrix", x => x.accountMatrixId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "accountMatrix");
+        }
+    }
+}
