a08ed550538492488e3e4a28fefe607f96f34edf
masquernya <127450910+masquernya@users.noreply.github.com>
2023-08-06 18:40:18 -0400

    Add avatar approval notice so people don't think your avatar broke

--- frontend/components/me/matrix.js
+++ frontend/components/me/matrix.js
@@ -1,12 +1,25 @@
 import s from "./me.module.css";
 import api from "../../lib/api";
 import {useState} from "react";
+import modal from '../../styles/Modal.module.css';
 
 export default function Matrix({matrix, setMatrix, discord, setAvatar}) {
   const [feedback, setFeedback] = useState(null);
   const [matrixToAdd, setMatrixToAdd] = useState('');
+  const [showAvatarWarning, setShowAvatarWarning] = useState(false);
 
   return <div className={s.meSection}>
+    {
+      showAvatarWarning ? <div className={modal.modal}>
+        <div className={modal.content}>
+          <h3 className='fw-bold'>Avatar Approval</h3>
+          <p>It may take a few minutes for your profile picture to be approved. Until then, or if your picture is rejected, your profile picture will show up as a question mark.</p>
+          <button className={s.saveButton} onClick={() => {
+            setShowAvatarWarning(false);
+          }}>Ok</button>
+        </div>
+      </div> : null
+    }
     <h4 className={s.subtitle + ' text-uppercase'}>Matrix</h4>
     {
       feedback ? <p className='text-danger'>{feedback}</p> : null
@@ -29,6 +42,7 @@ export default function Matrix({matrix, setMatrix, discord, setAvatar}) {
           setMatrixToAdd(e.currentTarget.value);
         }}/>
         <button className={s.saveButton} onClick={() => {
+          setFeedback(null);
           api.request('/api/user/Matrix', {
             method: 'POST',
             body: {
@@ -40,10 +54,20 @@ export default function Matrix({matrix, setMatrix, discord, setAvatar}) {
               setMatrix(data.body);
               // Get new avatar. It's possible it was updated.
               api.request('/api/user/Avatar').then(data => {
+                if (!data.body)
+                  setShowAvatarWarning(true); // Avatar was not auto approved
                 setAvatar(data.body);
               });
             })
           }).catch(err => {
+            if (err.code === 'InvalidMatrixUsernameException') {
+              setFeedback('Username is invalid. Make sure you include the domain, such as "matrix.org".');
+              return;
+            }
+            if (err.code === 'MatrixUserNotFoundException') {
+              setFeedback('User does not exist.');
+              return;
+            }
             setFeedback(err.message);
           })
         }}>Link Account</button>
