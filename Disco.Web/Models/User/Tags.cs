fd9e128518fb1d3ead248422d2dc547cd9eae79e
masquernya <127450910+masquernya@users.noreply.github.com>
2023-08-06 14:13:04 -0400

    Add models for top tag admin

--- Disco.Web/Models/User/Tags.cs
+++ Disco.Web/Models/User/Tags.cs
@@ -9,3 +9,20 @@ public class RemoveTagRequest
 {
     public long tagId { get; set; }
 }
+
+public class TopTagWithCount
+{
+    public string tag { get; set; }
+    public long count { get; set; }
+}
+
+public class ApproveTopTagRequest
+{
+    public string tag { get; set; }
+    public string displayTag { get; set; }
+}
+
+public class DeleteTopTagRequest
+{
+    public string tag { get; set; }
+}
