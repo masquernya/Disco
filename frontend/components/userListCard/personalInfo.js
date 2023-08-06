0d7e7c9f6e16fc5ef1943a3675344aec1f45c14f
masquernya <127450910+masquernya@users.noreply.github.com>
2023-08-06 18:39:45 -0400

    Redesign

--- frontend/components/userListCard/personalInfo.js
+++ frontend/components/userListCard/personalInfo.js
@@ -1,10 +1,18 @@
 import s from './userListCard.module.css';
+import { FaCalendar } from "@react-icons/all-files/fa/FaCalendar";
+import { FaGenderless } from '@react-icons/all-files/fa/FaGenderless';
+
+
 export default function PersonalInfo({age, gender, pronouns}) {
   return <div className={s.personalContainer}>
-    {age ? age : null}
-    {age && (gender || pronouns) ? "|" : null}
-    {gender ? gender : null}
-    {gender && pronouns ? '|' : null}
+    {age ? <div>
+      <FaCalendar /> <span className={s.age}>{age} Years Old</span>
+    </div> : null}
+
+    {gender ? <span className='mt-2'>
+      <span>{gender}</span>
+    </span> : null}
+    {gender && pronouns ? <FaGenderless className={s.genderPronounSep} /> : null}
     {pronouns ? pronouns : null}
   </div>
 }
