e94484286e772db663c0bc9f82b64163f4857bc1
masquernya <127450910+masquernya@users.noreply.github.com>
2023-08-06 13:27:33 -0400

    add ability to update matrix avatar

--- frontend/components/me/avatar.js
+++ frontend/components/me/avatar.js
@@ -3,27 +3,50 @@ import api from "../../lib/api";
 import {useState} from "react";
 import {getUser} from "../../lib/globalState";
 
-export default function Avatar({avatar, setAvatar}) {
+export default function Avatar({avatar, setAvatar, discord, matrix}) {
   const [feedback, setFeedback] = useState(null);
+  const [settingAvatar, setSettingAvatar] = useState(false);
 
   return <div className={s.meSection}>
     <h4 className={s.subtitle + ' text-uppercase'}>Avatar</h4>
-    <p className={s.subtitleHelp}>Your avatar is fetched from discord when you link your account. You cannot set a custom avatar. If you changed your avatar after linking your account, you can click the button below to update your avatar.</p>
+    <p className={s.subtitleHelp}>Your avatar is fetched from Discord or Matrix when you link your account. You cannot set a custom avatar. If you changed your avatar after linking your account, you can click the button below to update your avatar.</p>
     {
       feedback ? <p className='text-danger'>{feedback}</p> : null
     }
     <div>
-      <button className={s.saveButton} onClick={() => {
-        api.request('/api/user/DiscordLinkUrl', {
-          method: 'POST',
-        }).then(data => {
-          window.location.href = data.body.redirectUrl;
-        })
-      }}>Update Avatar</button>
+      {
+        discord ? <button disabled={settingAvatar} className={s.saveButton} onClick={() => {
+          setSettingAvatar(true);
+          api.request('/api/user/DiscordLinkUrl', {
+            method: 'POST',
+          }).then(data => {
+            window.location.href = data.body.redirectUrl;
+          })
+        }}>Update Avatar (Discord)</button> : null
+      }
+      {
+        matrix ? <button disabled={settingAvatar} className={s.saveButton} onClick={() => {
+          setSettingAvatar(true);
+          api.request('/api/user/Matrix', {
+            method: 'POST',
+            body: {
+              username: '@' + matrix.name + ':' + matrix.domain,
+            }
+          }).then(data => {
+            return api.request('/api/user/Avatar').then(data => {
+              setAvatar(data.body);
+            });
+          }).catch(err => setFeedback(err.message))
+            .finally(() => {
+              setSettingAvatar(false);
+            })
+
+        }}>Update Avatar (Matrix)</button> : null
+      }
     </div>
     <div>
       {avatar ?
-        <button className='btn btn-sm btn-outline-danger mt-4' onClick={() => {
+        <button disabled={settingAvatar} className='btn btn-sm btn-outline-danger mt-4' onClick={() => {
           api.request('/api/user/Avatar', {
             method: 'DELETE',
           }).then(() => {
