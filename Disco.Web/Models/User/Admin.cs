1458bb0050fbc923df649568f583011c01547ac0
masquernya <127450910+masquernya@users.noreply.github.com>
2023-08-08 23:04:23 -0400

    Make Account property required, add AccountMatrix to FullUserInfoResponse

--- Disco.Web/Models/User/Admin.cs
+++ Disco.Web/Models/User/Admin.cs
@@ -4,9 +4,14 @@ namespace Disco.Web.Models.User;
 
 public class FullUserInfoResponse
 {
+    public FullUserInfoResponse(Account account)
+    {
+        this.account = account;
+    }
     public Account account { get; set; }
-    public IEnumerable<AccountTag> accountTags { get; set; }
-    public AccountDescription description { get; set; }
-    public AccountDiscord discord { get; set; }
-    public AccountAvatar avatar { get; set; }
+    public IEnumerable<AccountTag> accountTags { get; set; } = ArraySegment<AccountTag>.Empty;
+    public AccountDescription? description { get; set; }
+    public AccountDiscord? discord { get; set; }
+    public AccountAvatar? avatar { get; set; }
+    public AccountMatrix? matrix { get; set; }
 }
