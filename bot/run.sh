c7de95fcb6449d49093a81be29fc809c3bca1819
masquernya <127450910+masquernya@users.noreply.github.com>
2023-09-10 14:48:45 -0400

    Restart on failure instead of rm

--- bot/run.sh
+++ bot/run.sh
@@ -1,3 +1,3 @@
 mkdir -p ./storage;
 # Detached, mount ./config and ./storage
-docker run -d --rm -it --init -v $(pwd)/config:/bot/config -v $(pwd)/storage:/bot/storage disco-matrix-bot:latest
+docker run -d --restart=on-failure -it --init -v $(pwd)/config:/bot/config -v $(pwd)/storage:/bot/storage disco-matrix-bot:latest
