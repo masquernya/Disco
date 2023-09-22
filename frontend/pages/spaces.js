42db6dafc331def6b75075aedce52dd93d3112f9
masquernya <127450910+masquernya@users.noreply.github.com>
2023-09-22 16:42:57 -0400

    Add spaceId query param

--- frontend/pages/spaces.js
+++ frontend/pages/spaces.js
@@ -2,13 +2,16 @@ import {useState} from "react";
 import Head from "next/head";
 import MatrixSpaces from "../components/matrixSpaces";
 import api from "../lib/api";
+import {useRouter} from "next/router";
 
 export default function Spaces(props) {
+  const router = useRouter();
+
   return <>
     <Head>
       <title>Matrix Spaces - DiscoFriends</title>
     </Head>
-    <MatrixSpaces spaces={props.spaces} />
+    <MatrixSpaces spaces={props.spaces} spaceId={router.query.id} />
   </>
 }
 
