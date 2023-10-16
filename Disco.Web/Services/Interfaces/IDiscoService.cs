17625cc5ce793aa4b1d97452516b16845616c0cb
masquernya <127450910+masquernya@users.noreply.github.com>
2023-10-16 11:38:06 -0400

    Add IDiscoService, ITransientDiscoService<T>, ISingletonDiscoService<T>

---
+++ Disco.Web/Services/Interfaces/IDiscoService.cs
@@ -0,0 +1,16 @@
+namespace Disco.Web.Services;
+
+public interface IDiscoService
+{
+    
+}
+
+public interface ITransientDiscoService<T>
+{
+    
+}
+
+public interface ISingletonDiscoService<T>
+{
+    
+}
